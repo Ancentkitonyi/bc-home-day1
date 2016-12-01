05140674130f78924771db3294a7fe75cb3a4591
Ancentkitonyi <ancentkitonyi.m@gmail.com>
2016-12-01 12:49:27 +0300

    changed type to isinstance

--- data_type/data_type.py
+++ data_type/data_type.py
@@ -1,17 +1,19 @@
 def data_type(data):
-  if type(data) == str:
-   return len(data)
-  elif data is None:
-    return 'no value'
-  elif type(data) == bool:
-    return data
-  elif type(data) == int:
-    if data < 100:
-      return 'less than 100'
-    elif data > 100:
-      return 'more than 100'
-    else:
-      return 'equal to 100'
-#  elif isinstance(data,list):
-  #    return data[2]
-      
+    if isinstance(data, str):
+        return len(data)
+    elif n is None:
+        return 'no value'
+    elif isinstance(data, bool):
+        return data
+    elif isinstance(data, int):
+        if data < 100:
+            return 'less than 100'
+        elif data == 100:
+            return 'equal to 100'
+        elif data > 100:
+            return 'more than 100'
+    elif isinstance(data, list):
+        if 3 <= len(data):
+            return data[2]
+        else:
+          return None
